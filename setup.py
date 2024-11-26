6be11797087a0490e7e83218ad3955db974b61a3
xinlabert <xinalbert88@gmail.com>
2024-11-26 10:51:24 +0800

    change some code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='axtoolkit',
-    version='0.1.2',
+    version='0.1.3',
     description='A toolkit for analyzing and processing data',
     author='AlbertXin',
     author_email='<albert_xin@qq.com>',
