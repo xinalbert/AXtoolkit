87656ce128f73085bce77c780adac827155a89fb
xinlabert <xinalbert88@gmail.com>
2024-11-26 09:36:00 +0800

    make some changes to setup.py and .github/workflows/publish-to-conda.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='axtoolkit',
-    version='0.1.0',
+    version='0.1.1',
     description='A toolkit for analyzing and processing data',
     author='AlbertXin',
     author_email='<albert_xin@qq.com>',
