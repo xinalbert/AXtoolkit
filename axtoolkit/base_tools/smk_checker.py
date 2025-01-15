09b8a7411d2fda1d63cb4aae92bc51a3e591cf75
xinlabert <xinalbert88@gmail.com>
2025-01-15 11:53:11 +0800

    fix some bugs of smk_checker.py

--- axtoolkit/base_tools/smk_checker.py
+++ axtoolkit/base_tools/smk_checker.py
@@ -5,17 +5,22 @@ import subprocess
 class command_Error(Exception):
     pass
 
-def run_cmd(cmd_str):
+def run_cmd(cmd_str,checknum = 0):
     """
     run a command in shell
     """
     try:
-        subprocess.run(cmd_str, shell=True, check=True)
+        process = subprocess.Popen(cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        if process.returncode== checknum:
+            pass
+        else:
+            raise command_Error(f"Execution failed for command: {cmd_str}\nError: {err.decode()}")
     except Exception as e:
         raise command_Error(f"Execution failed for command: {cmd_str}\nError: {e}")
 
 
-def cmd_check(work_dir, cmd_file_name, cmd_list):
+def cmd_check(work_dir, cmd_file_name, cmd_list, checknum = 0):
     """
     check if command has been executed before, if not, write\
      command to file and execute it.
@@ -26,6 +31,7 @@ def cmd_check(work_dir, cmd_file_name, cmd_list):
         work_dir: working directory
         cmd_file_name: command file name
         cmd_list: list of commands to be executed
+        checknum: check number of return code
     Returns:
         True if command has been executed, False if command has not been executed.
     must contain dirs: is work_dir/.cmd and work_dir/.cmd_ok
@@ -41,7 +47,7 @@ def cmd_check(work_dir, cmd_file_name, cmd_list):
     # 执行命令
     for cmd_str in cmd_list:
         # print(cmd_str)
-        run_cmd(cmd_str)
+        run_cmd(cmd_str, checknum = checknum)
     # 标记命令已执行
 
     print(f"Commands executed and file created : {cmd_file_name}")
